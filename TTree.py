95a09e23fd8d1f55b65327c1de235d180b0be368
Vivian Link <vivian.link@sunrise.ch>
2021-09-09 14:26:35 -0700

    added limix tests

---
+++ TTree.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 17:43:29 2021
+
+@author: linkv
+"""
+import numpy as np
+import tskit as ts
+
+# class TTrees:
+#     def __init__(self, ts_object):
+#         self.trees = ts_object.trees()
+#         self.number = ts_object.num_trees
+        
+#     # def make(X):
+#     #     X_ = (X+X.T)/2
+#     #     np.fill_diagonal(X_, 0)
+#     #     return X_
+
+#     # def diff(y, N):
+#     #     cols = np.tile(y, (N, 1))
+#     #     rows = cols.T
+#     #     buffer = cols - rows
+#     #     return np.abs(buffer)
+        
+
+
+#     # def solving_function(array, tree):
+#     #     covariance = trees_obj.TMRCA(0, N)
+#     #     inv = np.linalg.inv(covariance)
+#     #     np.dot(inv, array)
+
+    
+    
+
+class TTree:
+    def __init__(self, tree_iterator, N):
+        self.tree = tree_iterator
+        self.N = N
+        
+    def TMRCA(self, N):
+        """
+        see tskit to understand these classes: https://tskit.dev/tskit/docs/stable/python-api.html#the-tree-class
+        
+        - the goal of this function is to get pairwise branch lengths between all nodes of the tree
+        - we go through all internal nodes and update the cells of all their descendants at once
+        - the tree.time of a node gets larger backwards in time
+        - the idea is to fill the TMRCA matrix with the total height of the tree and then to 
+        deduct for all descendants of one node the branches from farther back in time
+        for example, the terminal nodes will have all branch lengths that connect their ancestors deducted 
+        -> they are only separated by the terminal branches
+        Since the height of the tree can only be known by finding the oldest node, which we don't know a priori 
+        we deduct the branch lengths from zero first, using the loop over nodes to find the height, and then add the height
+    
+        Parameters
+        ----------
+        tree : <class 'tskit.trees.Tree'>
+            The tree for which distance between samples should be calculated.
+        N : int
+            number of samples.
+    
+        Returns
+        -------
+        tmrca : matrix of dim ? and type ?
+            Time separating two samples.
+    
+        """
+        tmrca = np.zeros([N, N])
+        height = 0
+        for c in self.tree.nodes():
+            descendants = list(self.tree.samples(c))
+            n = len(descendants)
+            if(n == 0 or n == N): 
+                continue
+            t = self.tree.time(self.tree.parent(c)) - self.tree.time(c)
+            tmrca[np.ix_(descendants, descendants)] -= t
+            height = max(height, self.tree.time(self.tree.parent(c))) #time returns the time of a node
+        tmrca += height
+        return tmrca
+        
+    def solving_function(self, array):   
+        X = self.TMRCA(self.N)
+        covariance = (X+X.T)/2
+        np.fill_diagonal(covariance, 0)
+        inv = np.linalg.inv(covariance)
+        tmp = np.dot(inv, array)
+        # print("shape of my dot product",np.shape(tmp))
+        return(tmp)
+        
+        
+        
+        
+        
+        
+        
+        
+        
