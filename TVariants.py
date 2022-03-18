fixed bug with simulating causal variant with given allele freq

TVariants.py
@@ -178,6 +178,7 @@ class TVariantsFiltered(TVariants):
 
         """
         
+                
         # check if interval is valid
         num_lines = self.info[(self.info['position'] >= interval[0]) & (self.info['position'] <= interval[1])].shape[0]
         if num_lines == 0:
@@ -204,8 +205,8 @@ class TVariantsFiltered(TVariants):
         subplot.set(xlabel='Allele freq', ylabel='count', title = 'Histogram of af in requested interval and typed status')
 
         # find first variant with requested allele frequency
-        info = info_interval[np.greater_equal(info_interval['allele_freq'], freq)]
-
+        info = info_interval[np.isclose(info_interval['allele_freq'], freq)]
+        
         if info.shape[0] < 1:
             logfile.info("- Did not find locus with requested af " + str(freq) + ". Adapting af in increments of 0.001.")
         
@@ -221,7 +222,7 @@ class TVariantsFiltered(TVariants):
             while info.shape[0] < 1 and freq >= 0 and freq <= 0.5:
                 #remove or add small value to freq until a locus is found
                 freq = round(freq + step,3)                        
-                info = info_interval[np.greater_equal(info_interval['allele_freq'], freq)]
+                info = info_interval[np.isclose(info_interval['allele_freq'], freq)]
             
             #if loop was left because out of bounds, search in other direction
             if freq < 0 or freq > 0.5:
@@ -233,7 +234,7 @@ class TVariantsFiltered(TVariants):
     
                 while info.shape[0] < 1 and freq >= 0 and freq <= 0.5:
                     freq = round(freq + step,3)    
-                    info = info_interval[np.greater_equal(info_interval['allele_freq'], freq)]
+                    info = info_interval[np.isclose(info_interval['allele_freq'], freq)]
     
             if freq < 0 or freq > 0.5:
                 raise ValueError("Could not find locus with requested allele frequency")