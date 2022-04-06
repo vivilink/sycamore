07e1a2a1a0f8ad30de5146d04ecce152b81c1bd2
Vivian Link <vivian.link@sunrise.ch>
2022-04-05 18:07:06 -0700

    put back in line that was commented out

--- run_egrm.sh
+++ run_egrm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./egrm/build/scripts-3.9/trees2egrm --output ${2} ${3} --c-extension --skip-first-tree --output-format gcta ${1} --verbose
+./egrm/bin/trees2egrm --output ${2} ${3} --c-extension --skip-first-tree --output-format gcta ${1} --verbose
 
-#./egrm/bin/trees2egrm --output ${2} ${3} --c-extension --skip-first-tree --output-format numpy ${1} --verbose
+./egrm/bin/trees2egrm --output ${2} ${3} --c-extension --skip-first-tree --output-format numpy ${1} --verbose
 
 #~/git/argwas/egrm/bin/trees2egrm --output "/data/ARGWAS/experiments_N500/nullSimulation/REML/haploid/test" --c-extension  --skip-first-tree --output-format gcta --verbose /data/ARGWAS/experiments_N500/nullSimulation/REML/haploid/null_propTyped0.1_envNoise1_null_ploidy1_focal.trees
