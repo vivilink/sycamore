4c2c37f150d012c36c1ee0f4d742e4aa72c16b28
vlink <vivian.link@sunrise.ch>
2022-10-06 08:51:10 -0700

    made fixture for tree simulation

--- test/test_TAssociationTesting.py
+++ test/test_TAssociationTesting.py
@@ -7,6 +7,22 @@ import TAssociationTesting as at
 import stdpopsim
 import numpy as np
 
+@pytest.fixture(scope="package")
+def simulate():
+    # simulate full trees
+    species = stdpopsim.get_species("HomSap")
+    contig = species.get_contig("chr22")
+    model = species.get_demographic_model("OutOfAfrica_3G09")
+    samples = model.get_samples(20, 0, 0)
+    engine = stdpopsim.get_engine("msprime")
+    trees_full = engine.simulate(model, contig, samples, discrete_genome=True)
+
+    # extract an interval
+    trees_interval = [100000, 200000]
+    interval = np.array(trees_interval)
+    trees = trees_full.keep_intervals([interval], simplify=True)
+
+    return trees, trees_interval
 
 class TestAssociationTesting:
     @pytest.mark.parametrize(
@@ -15,19 +31,8 @@ class TestAssociationTesting:
          (30000, [130000, 160000, 190000, 200000]),
          ],
     )
-    def test_get_window_ends(self, window_size, window_ends_true):
-        # simulate full trees
-        species = stdpopsim.get_species("HomSap")
-        contig = species.get_contig("chr22")
-        model = species.get_demographic_model("OutOfAfrica_3G09")
-        samples = model.get_samples(20, 0, 0)
-        engine = stdpopsim.get_engine("msprime")
-        trees_full = engine.simulate(model, contig, samples, discrete_genome=True)
-
-        # extract an interval
-        trees_interval = [100000, 200000]
-        interval = np.array(trees_interval)
-        trees = trees_full.keep_intervals([interval], simplify=True)
+    def test_get_window_ends(self, simulate, window_size, window_ends_true):
+        trees, trees_interval = simulate
 
         # get windows when skipping first tree, sequence length is multiple of window_size
         window_ends = at.get_window_ends(ts_object=trees, window_size=window_size, trees_interval=trees_interval)
